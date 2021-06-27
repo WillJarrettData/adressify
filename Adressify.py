0dccadbe9fefc957f6f3a45817ee9c9e2c1824c3
WillJarrett1 <73312768+WillJarrett1@users.noreply.github.com>
2021-06-27 16:03:39 -0400

    Update Adressify.py

--- Adressify.py
+++ Adressify.py
@@ -181,7 +181,7 @@ if pressed:
     #wait until the page loads
     try:
         element = WebDriverWait(driver, 100).until(
-        EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[4]/div[2]/div[1]/span[3]"))
+        EC.presence_of_element_located((By.CLASS_NAME, "pad-15"))
             )
     except:
         pass
