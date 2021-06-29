784b8a6850dcd2bab345498d33b5261aead9b321
WillJarrett1 <73312768+WillJarrett1@users.noreply.github.com>
2021-06-29 13:26:42 -0400

    Update Adressify.py

--- Adressify.py
+++ Adressify.py
@@ -112,6 +112,7 @@ if pressed:
     options.add_argument("--headless")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
+    options.add_argument('--ignore-certificate-errors')
     options.add_argument("window-size=1400,900")
     options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
